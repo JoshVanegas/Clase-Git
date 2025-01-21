1f63a2223ac7269c47e53ca17631f0916beadd8e
Josh Vanegas <vanegasmejiajosepharmando@gmail.com>
2025-01-21 17:51:33 -0500

    Carga

--- Arrays/main.js
+++ Arrays/main.js
@@ -1,15 +1,30 @@
 const tarea = document.getElementById("tarea")
 const tabla = document.querySelector("table")
+const body = document.getElementById("body")
+const p = document.getElementById("parrafo")
+
 let arrayt = []
+let con = 0
+
 document.getElementById("btn1").addEventListener("click", () => {
 
-    let con = 0
-    let t = tarea.value
-    if(arrayt.length > 4){
-        arrayt.push(tarea)
-        tabla.innerHTML= `<tr> <td>  </td> ${arrayt[con]} <td> </td> </tr>`
+    if(con < 5 && tarea.value != ""){
+        arrayt.push(tarea.value)
+        body.innerHTML= `${body.innerHTML}<tr><td>${con+1}</td><td>${arrayt[con]}</td></tr>`
+        tabla.style.display = "block"
         con++
+        p.innerText = ""
+    } else if(con >= 5){
+        p.innerText = "Limite de tareas alcanzados"
     }
-    tabla.style.display = "flex"
+})
+document.getElementById("btn2").addEventListener("click", () => {
+
+    arrayt.pop()
+    con--
+    if (arrayt.length == 0  && con == 0){
+        tabla.style.display = "none"
+        p.innerHTML = "Sin tareas por el momento"
+    }
 
 })
